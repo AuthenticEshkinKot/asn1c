af89519f56297ee478056ece9cec60610155132a
Maxim Kuzmin <maxriderg@gmail.com>
2024-09-11 18:25:44 +0300

    Fixed some warnings

--- skeletons/INTEGER.c
+++ skeletons/INTEGER.c
@@ -773,8 +773,8 @@ INTEGER_decode_aper(asn_codec_ctx_t *opt_codec_ctx, asn_TYPE_descriptor_t *td,
 					? asn_ulong2INTEGER(st, value)
 					: asn_long2INTEGER(st, value))
 					ASN__DECODE_FAILED;
-					ASN_DEBUG("Got value %lld + low %lld",
-							  value, ct->lower_bound);
+				ASN_DEBUG("Got value %lld + low %lld",
+							value, ct->lower_bound);
 			}
 			return rval;
 		} else {
